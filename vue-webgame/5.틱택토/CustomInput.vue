832ce031a2b241ca104d9b4f4a45b491d64c62b4
heroyooi <heroyooi1018@gmail.com>
2021-05-06 16:24:25 +0900

    자식 컴포넌트에서 부모 컴포넌트로 값 보내는 예제 추가

---
+++ vue-webgame/5.틱택토/CustomInput.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <input type="text" v-model="inputValue" :placeholder="placeholder" @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      required: true,
+    },
+    customOnInput: {
+      type: Function,
+      required: false,
+    },
+  },
+  data() {
+    return {
+      inputValue: '',
+    };
+  },
+  methods: {
+    onInput() {
+      if (this.customOnInput) {
+        this.customOnInput(this.inputValue);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  margin-top: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+}
+input {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #666;
+  box-sizing: border-box;
+}
+</style>
--- vue-webgame/5.틱택토/TicTacToe.vue
+++ vue-webgame/5.틱택토/TicTacToe.vue
@@ -3,15 +3,25 @@
     <div>{{ turn }}님의 턴입니다.</div>
     <table-component :table-data="tableData" />
     <div v-if="winner">{{ winner }}님의 승리!</div>
+
+    <!-- eventHandler -->
+    <custom-input placeholder="커스텀 인풋" :customOnInput="customOnInput" />
+
+    <!-- $emit -->
+    <custom-input-emit placeholder="커스텀 인풋 emit" @cinput="customOnInputEmit" />
   </div>
 </template>
 
 <script>
 import TableComponent from './TableComponent';
+import CustomInput from './CustomInput';
+import CustomInputEmit from './CustomInputEmit';
 
 export default {
   components: {
     TableComponent,
+    CustomInput,
+    CustomInputEmit,
   },
   data() {
     return {
@@ -28,6 +38,12 @@ export default {
     onChangeData() {
       // this.tableData[1][0]] = 'X'; 작동하지 않음
       this.$set(this.tableData[1], 0, 'X'); // Vue.set과 동일
+    },
+    customOnInput(state) {
+      console.log('자식 컴포넌트에서 보낸 값 : ', state);
+    },
+    customOnInputEmit(state) {
+      console.log('자식 컴포넌트에서 보낸 값 : ', state);
     },
   },
 };
