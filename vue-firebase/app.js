d90f441212ad95199a04db61b4bb3a7fb1b57598
heroyooi <heroyooi1018@gmail.com>
2021-06-17 09:46:40 +0900

    새로운 Vue 공부

--- vue-firebase/app.js
+++ vue-firebase/app.js
@@ -1,18 +1,40 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: 'http://heroyooi.ivyro.net',
       showBooks: true,
-      title: 'The Final Empire',
-      author: 'Brandon Sanderson',
-      age: 45,
+      x: 0,
+      y: 0,
+      books: [
+        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true },
+      ]
     };
   },
   methods: {
-    changeTitle(title) {
-      // this.title = 'Words of Radiance';
-      this.title = title;
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
     },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) {
+        console.log(data)
+      }
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    }
   },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav)
+    }
+  }
 });
 
 app.mount('#app');
--- vue-firebase/readme.md
+++ vue-firebase/readme.md
@@ -1,3 +1,6 @@
 # Vue Firebase
 
-- [강좌1 | 23:20](https://www.youtube.com/watch?v=F7PLPJqVotk&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=3)
+## 참고 링크
+
+- [강좌](https://www.youtube.com/watch?v=GWRvrSqnFbM&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=4)
+- [저장소](https://github.com/iamshaunjp/Vue-3-Firebase)
