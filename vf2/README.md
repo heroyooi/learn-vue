470dd7db24e1b76db056c6ea5d31915408b1b574
heroyooi <heroyooi1018@gmail.com>
2021-07-04 23:38:50 +0900

    계속해서 공부중

--- vf2/README.md
+++ vf2/README.md
@@ -1,24 +1,42 @@
 # vf2
 
-## Project setup
-```
-npm install
+## 프로젝트 설치
+```command
+npm i -g @vue/cli
+vue --version
+vue create vf2
 ```
 
-### Compiles and hot-reloads for development
-```
+- Manually select features
+- Babel, Router, Vuex, Linter
+- ESLint Standard
+
+```command
+cd vf2
 npm run serve
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
+## 8. vuetify 설치하기
+```command
+vue add vuetify
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+- ? Choose a preset: Configure (advanced)
+- ? Use a pre-made template? (will replace App.vue and HelloWorld.vue) Yes
+- ? Use custom theme? No
+- ? Use custom properties (CSS variables)? No
+- ? Select icon font Material Design Icons
+- ? Use fonts as a dependency (for Electron or offline)? No
+- ? Use a-la-carte components? Yes (자기가 사용한 것만 영리하게 잡아내서 설치한다.)
+- ? Select locale English
+
+- IE11 & Safari 9 support
+- [브라우저 호환성](https://v2.vuetifyjs.com/ko/getting-started/browser-support/#ie-11-amp-safari-9-support)
+
+## 9. appbar 만들기
+
+- [Vuetify > UI 컴포넌트 > App bars](https://v2.vuetifyjs.com/ko/components/app-bars)
+
+## 참고링크
+- [Vuetify 공식문서](https://v2.vuetifyjs.com/ko)
+- [강좌 | Vue와 Firebase로 나만의 사이트 만들기 12](https://www.youtube.com/watch?v=ShRxf-bCbK0&list=PLjpTKic1SLZsWckh_DZ6tYH17MM6hBAc7&index=13)
--- vf2/src/App.vue
+++ vf2/src/App.vue
@@ -1,12 +1,51 @@
 <template>
-  <div id="app">
-    title 수정
-    <div id="nav">
-      <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> |
-      <router-link to="/xxx">xxx</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title>Page title</v-toolbar-title>
+      <v-spacer/>
+      <v-btn icon to="/about">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon to="/">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+    </v-navigation-drawer>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer app color="primary" dark absolute>
+      <v-spacer></v-spacer>
+      <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+  </v-app>
 </template>
 
-<style></style>
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      drawer: false
+    }
+  }
+}
+</script>
